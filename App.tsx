97970a9c88c25459a90143c0b25d70da69dd5f4e
Vladi-Ciuculescu <vladimir.ciuculescu@gmail.com>
2023-05-22 20:38:00 +0300

    Added prettier

--- App.tsx
+++ App.tsx
@@ -1,5 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Switch } from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Switch,
+  TextInput,
+  ScrollView,
+  FlatList,
+} from 'react-native';
 import {
   Text,
   View,
@@ -7,24 +14,65 @@ import {
   HStack,
   Button,
   TextField,
+  Divider,
 } from 'native-base';
+import { useState } from 'react';
+
+interface Goal {
+  text: string;
+  id: string;
+}
 
 export default function App() {
+  const [input, setInput] = useState<string>('');
+  const [goals, setGoals] = useState<Goal[]>([]);
+
+  const addGoal = () => {
+    setGoals((prevGoals) => [
+      ...prevGoals,
+      { text: input, id: Math.random.toString() },
+    ]);
+    setInput('');
+  };
+
   return (
     <NativeBaseProvider>
       <SafeAreaView>
-        <StatusBar style='dark' />
-        <View padding={50}>
-          <HStack justifyContent='space-between'>
-            <TextField
-              style={{ height: 100 }}
-              value=''
-              placeholder='awd'
-              width={'70%'}
+        <StatusBar style="dark" />
+
+        <View style={{ paddingHorizontal: 10, height: '100%' }}>
+          <HStack style={{ width: '100%', justifyContent: 'space-evenly' }}>
+            <TextInput
+              style={styles.textInput}
+              value={input}
+              onChangeText={(e) => setInput(e)}
             />
-            <Button>Add Goal</Button>
+            <Button onPress={addGoal}>Add Goal</Button>
           </HStack>
-          <View></View>
+          <Divider my={5} />
+          <View>
+            <Text>List of goals</Text>
+            <View>
+              {goals.length !== 0 ? (
+                <FlatList
+                  alwaysBounceVertical={false}
+                  data={goals}
+                  renderItem={(itemData) => {
+                    return (
+                      <View style={styles.goalItem} key={itemData.item.id}>
+                        <Text style={styles.goalText}>
+                          {itemData.item.text}
+                        </Text>
+                      </View>
+                    );
+                  }}
+                  keyExtractor={(itemData) => itemData.id}
+                />
+              ) : (
+                <Text>There are no goals </Text>
+              )}
+            </View>
+          </View>
         </View>
       </SafeAreaView>
     </NativeBaseProvider>
@@ -32,10 +80,18 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  textInput: {
+    width: '70%',
+    borderWidth: 1,
+    borderColor: 'blue',
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: '#5e0acc',
+  },
+  goalText: {
+    color: 'white',
   },
 });
